90bf13583a0ce923d1355bdf9269b903f53cb426
JooYB <156300157+JooYB@users.noreply.github.com>
2024-01-16 14:40:11 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import streamlit as st
-#from dotenv import load_dotenv
-from langchain_openai.llms import OpenAI
-from langchain_openai.chat_models import ChatOpenAI
+
+#from langchain_core.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
 
 # Load environment variables
+#from dotenv import load_dotenv
 #load_dotenv()
 
 # Initialize LangChain and ChatOpenAI
-llm = OpenAI()
+
 chat_model = ChatOpenAI()
 
-content = "ESG 공급망"
+content1 = "ESG"
 content2 = "한국"
 title = st.text_input("문의 사항을 입력해주세요!")
 
@@ -18,21 +19,15 @@ title = st.text_input("문의 사항을 입력해주세요!")
 if st.button("검색하기"):
     with st.spinner("Searching..."):
         # Combine input content
-        combined_content = content + content2 + title
+        combined_content = content1 + content2 + title
 
-        # Invoke with LangChain
-        result = llm.invoke(combined_content)
+        #result = chat_model.invoke(combined_content)
+        result = chat_model.predict(combined_content)
 
-        # Invoke with ChatOpenAI
-        result2 = chat_model.invoke(combined_content)
-
-        # Create a Markdown representation of the conversation
-        # conversation = f"**User Input:**\n{combined_content}\n\n**LangChain Result:**\n{result}\n\n**ChatOpenAI Result:**\n{result2}"
-
-        # Print the conversation
-        st.title("공급망AS")
-
-        st.write("인공지능 답변입니다. ", result)
-        st.write("채팅 답변입니다. ", result2)
+        st.title("공급망 https://www.esgsupport.or.kr")
+        
+        #st.write("인공지능 답변입니다. ", result)
+        #st.write("채팅 답변입니다. ", html_content)
+        st.markdown(result, unsafe_allow_html=True)
 else:
     st.write("취소")
